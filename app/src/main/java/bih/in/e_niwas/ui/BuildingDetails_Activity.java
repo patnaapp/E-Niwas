7578eadbe57b204749eba146a9daa85ffbb7410a
patnaapp <patnaapp@gmail.com>
2020-10-06 18:05:06 +0530

    updates

---
+++ app/src/main/java/bih/in/e_niwas/ui/BuildingDetails_Activity.java
@@ -0,0 +1,16 @@
+package bih.in.e_niwas.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.exp.e_niwas.R;
+
+public class BuildingDetails_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_building_details_);
+    }
+}
