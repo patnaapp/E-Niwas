c17a1c24950e4417b69be23a4f028a11921177a5
patnaapp <patnaapp@gmail.com>
2020-10-07 11:11:54 +0530

    updates

--- app/src/main/java/bih/in/e_niwas/ui/NewEntryForm_Activity.java
+++ app/src/main/java/bih/in/e_niwas/ui/NewEntryForm_Activity.java
@@ -2,15 +2,34 @@ package bih.in.e_niwas.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.exp.e_niwas.R;
 
 public class NewEntryForm_Activity extends AppCompatActivity {
 
+    Button btn_proceed;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_entry_form_);
+
+        btn_proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(NewEntryForm_Activity.this,BuildingDetails_Activity.class);
+                startActivity(i);
+
+            }
+        });
+    }
+
+    public void initialisation()
+    {
+        btn_proceed=findViewById(R.id.btn_proceed);
     }
 }
