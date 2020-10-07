b793c3b71c6cbf841edd8370d97182afc841eb46
patnaapp <patnaapp@gmail.com>
2020-10-07 13:17:12 +0530

    updates

--- app/src/main/java/bih/in/e_niwas/ui/BuildingDetails_Activity.java
+++ app/src/main/java/bih/in/e_niwas/ui/BuildingDetails_Activity.java
@@ -3,10 +3,20 @@ package bih.in.e_niwas.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.exp.e_niwas.R;
 
 public class BuildingDetails_Activity extends AppCompatActivity {
+
+    EditText edt_building_name,edt_plinth_area,edt_builtup_area,edt_ofc_detail,edt_building_year,edt_remarks;
+    CheckBox chk_judicial,chk_non_judicial,chk_residential,chk_non_residential,chk_gazetted,chk_non_gazetted,chk_mixedd;
+    Spinner sp_buildingtype,sp_poolof_building,sp_buildin_status;
+    ImageView img1,img2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/bih/in/e_niwas/ui/NewEntryForm_Activity.java
+++ app/src/main/java/bih/in/e_niwas/ui/NewEntryForm_Activity.java
@@ -6,12 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.exp.e_niwas.R;
 
 public class NewEntryForm_Activity extends AppCompatActivity {
 
     Button btn_proceed;
+    Spinner sp_building_div,sp_dist,sp_subdiv,sp_block,sp_ward_pan,sp_bulding_check;
+    CheckBox chk_open_land,chk_existing_building,chk_urban,chk_rural;
+    EditText edt_pincode,edt_thana,edt_khata,edt_khesra,edt_nrth_chauhaddi,edt_south_chauhaddi,edt_neast_chauhaddi,edt_west_chauhaddi,edt_land_area,edt_trees_no,et_trees_details;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
